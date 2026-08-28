9768162e46a7c92278c58e61f3967887b36ec436
thepulsestation <thepulsestation@gmail.com>
2026-08-28 16:58:04 +0200

    Actualizar caché de la PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'en-casa-v1';
+const CACHE_VERSION = 'en-casa-v2';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
