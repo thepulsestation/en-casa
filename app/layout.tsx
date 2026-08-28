407cf977346c189b6c0f0f5e9c6c463314c88cad
thepulsestation <thepulsestation@gmail.com>
2026-08-28 17:21:01 +0200

    Usar la barra de estado segura en iPhone

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    statusBarStyle: 'black-translucent',
+    statusBarStyle: 'default',
     title: 'En casa',
   },
 };
