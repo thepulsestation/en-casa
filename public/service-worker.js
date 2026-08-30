67718fed2c1c216848d54c64c01e507a535d3baf
thepulsestation <thepulsestation@gmail.com>
2026-08-30 12:56:58 +0200

    Actualizar caché de la aplicación

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'en-casa-v3';
+const CACHE_VERSION = 'en-casa-v4';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
