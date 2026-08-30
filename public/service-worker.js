4007be96d9a7bab07535d095a68a660e80f8a5d9
thepulsestation <thepulsestation@gmail.com>
2026-08-30 17:56:57 +0200

    Actualizar caché para Deshacer

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'en-casa-v5';
+const CACHE_VERSION = 'en-casa-v6';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
