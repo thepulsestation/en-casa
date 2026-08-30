88c83b05bf907af8cc3798722e6c7b98cbc7ac2b
thepulsestation <thepulsestation@gmail.com>
2026-08-30 17:42:15 +0200

    Actualizar caché para el filtro Otro

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'en-casa-v4';
+const CACHE_VERSION = 'en-casa-v5';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
